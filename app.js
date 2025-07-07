1510640bb86604846212b52e4139e81aebbac5b5
Patric <patricmorales1997@gmail.com>
2025-07-07 19:21:19 -0300

    Atualização do arquivo

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const livroRoutes = require('./routes/livros');
 const clienteRoutes = require('./routes/clientes');
+const emprestimoRoutes = require('./routes/emprestimos');
 
 app.use(express.json());
 app.use('/livros', livroRoutes);
 app.use('/clientes', clienteRoutes);
+app.use('/emprestimos', emprestimoRoutes);
 
 module.exports = app;
