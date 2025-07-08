0cc30e3c5d7fae51ddb6a36aa5da41332a916757
Patric <patricmorales1997@gmail.com>
2025-07-08 14:46:00 -0300

    Atualização

--- routes/emprestimoRoute.js
+++ routes/emprestimoRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const emprestimoController = require('../controllers/EmprestimoController');
+const emprestimoController = require('../controllers/emprestimoController');
 
 router.get('/', emprestimoController.listarEmprestimos);
 router.get('/:id', emprestimoController.buscarEmprestimoPorId);
