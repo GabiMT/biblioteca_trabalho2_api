a41aec038b18cc0b613cf388b2be9b3d7b1bff24
Patric <patricmorales1997@gmail.com>
2025-07-08 15:18:55 -0300

    Atualização

--- routes/emprestimoRoute.js
+++ routes/emprestimoRoute.js
@@ -6,6 +6,7 @@ router.get('/', emprestimoController.listarEmprestimos);
 router.get('/:id', emprestimoController.buscarEmprestimoPorId);
 router.post('/', emprestimoController.criarEmprestimo);
 router.put('/devolver/:id', emprestimoController.devolverLivro);
+router.put('/:id', emprestimoController.atualizarEmprestimo);
 router.delete('/:id', emprestimoController.deletarEmprestimo);
 
 module.exports = router;
